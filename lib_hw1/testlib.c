618451e68998d07d8a9e02dc94808f93278987ad
sanglee <pksll325@gmail.com>
2019-09-26 21:02:26 -0700

    FUNC: create, dumpdata

--- lib_hw1/testlib.c
+++ lib_hw1/testlib.c
@@ -22,16 +22,15 @@ int main() {
 }
 
 void read_command(char *input_str) {
+	// variables for reading commands
 	int i = 0;
 	int idx = 0, word_num = 0, token_idx = 0;
 	char *command;
-	char tokenize[MAX_INPUT_LEN][MAX_INPUT_LEN] = { 0 };
+	char tokenize[MAX_INPUT_LEN][MAX_INPUT_LEN] = { 0, };
 	bool word[MAX_INPUT_LEN] = { false };
-
+	
 	for(i = 0; i < strlen(input_str); i++) {
-		if(('!' <= input_str[i] && input_str[i] <= '~')) {
-			word[i] = true;
-		}
+		if(input_str[i] != ' ' && input_str[i] != '\n') word[i] = true;
 	}
 
 	for(i = 1; i < strlen(input_str); i++) {
@@ -42,8 +41,8 @@ void read_command(char *input_str) {
 
 	if(word_num > 0) {
 		idx = 0;
-		for(i = 0; i < strlen(input_str); i++){
-			if(input_str[i]) {
+		for(i = 0; i < strlen(input_str); i++) {
+			if(input_str[i] && word[i] == true) {
 				tokenize[idx][token_idx] = input_str[i];
 				token_idx++;
 			}
@@ -56,26 +55,61 @@ void read_command(char *input_str) {
 	}
 
 	command = tokenize[0];
+	
+	// input token check
+	/*
+	printf("%s\n", command);
+	for(i = 0; i<idx; i++) {
+		printf("%s\n", tokenize[i]);
+	}
+	*/	
 
-	if(!strcmp(command, "quit")){
+	if(!strcmp(command, "quit")) {
 		command_quit();
 	}
 	else if(!strcmp(command, "create")){
-		if(!strcmp(tokenize[1], "bitmap")){
+		if(!strcmp(tokenize[1], "bitmap")) {
 			create_bitmap(tokenize[2]);
 		}
-		else if(!strcmp(tokenize[1], "list")){
+		else if(!strcmp(tokenize[1], "list")) {
 			create_list(tokenize[2]);
 		}
-		else if(!strcmp(tokenize[1], "hashtable")){
+		else if(!strcmp(tokenize[1], "hashtable")) {
 			create_list(tokenize[2]);
 		}
+	}
+	else if(!strcmp(command, "dumpdata")) {
+		command_dumpdata(tokenize[1]);
+	}
+	else if(!strcmp(command, "list_insert")) {
+		struct ds_table *target = NULL;
+		int idx = atoi(tokenize[2]);
+		int data = atoi(tokenize[3]);
+
+		// find the name data structure
+		target = find_ds_table(tokenize[1]);
+		if(!target) return;
+
+		// new data to insert
+		struct list_item *new = malloc(sizeof(struct list_item));
+
+		new->data = data;
+
+		struct list *cur = target->list;
+		struct list_elem *ptr = list_head(cur);
+
+		for(int i = 0; i <= idx; i++) {
+			ptr = list_next(ptr);
+		}
+		
+		list_insert(ptr, &(new->elem));
+		
 	}
 
 }
 
 void command_quit() {
-	if(list_tab_head) {
+	if(ds_tab_head) {
 	}
 
 	exit_flag = true;
@@ -86,18 +120,19 @@ void create_bitmap(char *name) {
 }
 
 void create_list(char *name) {
-	struct list_table *new, *cur;
+	struct ds_table *new, *cur;
 
-	new = (struct list_table*)malloc(sizeof(struct list_table));
+	new = (struct ds_table*)malloc(sizeof(struct ds_table));
 	list_init(new->list);
 	strcpy(new->name, name);
+	new->type = 3;
 	new->next = NULL;
 
-	if(!list_tab_head) {
-		list_tab_head = new;
+	if(!ds_tab_head) {
+		ds_tab_head = new;
 	}
 	else {
-		cur = list_tab_head;
+		cur = ds_tab_head;
 		while(1) {
 			if(cur->next) {
 				cur = cur->next;
@@ -112,4 +147,59 @@ void create_list(char *name) {
 
 void create_hashtable(char *name) {
 	
+}
+
+struct ds_table *find_ds_table (char *name) {
+	struct ds_table *cur;
+	cur = ds_tab_head;
+
+	printf("FUNC: find_ds_table(%s)\n", name);
+
+	while(1) {
+		if(!strcmp(cur->name, name)) {
+			return cur;
+		}
+		else {
+			cur = cur->next;
+			if(cur == NULL) {
+				printf("ERROR: %s does not exists.\n", name);
+				break;
+			}
+		}
+	}
+
+	return NULL;
+}
+
+void command_dumpdata(char *name) {
+	struct ds_table *target = NULL;
+
+
+	target = find_ds_table(name);
+	if(target == NULL) return;
+
+	switch(target->type) {
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			dump_list(target->list);
+			break;
+	}
+}
+
+void dump_list(struct list* list) {
+	if(list_empty(list))
+		return;
+	else {
+		struct list_elem *cur;
+		struct list_item *target;
+
+		for(cur = list_begin(list); cur != list_end(list); cur = list_next(cur)) {
+			target = list_entry(cur, struct list_item, elem);
+			printf("%d ", target->data);
+		}
+		printf("\n");
+	}
 }
--- lib_hw1/testlib.h
+++ lib_hw1/testlib.h
@@ -11,20 +11,28 @@ struct list_item {
 	int data;
 };
 
-// table of list data_structure
-struct list_table {
+// table of data_structure
+struct ds_table {
 	char name[10];
 
+	//1: bitmap 2: hashtable 3: list
+	int type;
+
 	struct list* list;
-	struct list_table* next;
+	struct ds_table* next;
 };
 
-struct list_table *list_tab_head;
+struct ds_table *ds_tab_head;
 
 void read_command(char *input_str);
 
 void command_quit();
+void command_dumpdata(char *name);
 
 void create_bitmap(char *name);
 void create_list(char *name);
 void create_hashtable(char *name);
+
+void dump_list(struct list* list);
+
+struct ds_table *find_ds_table (char *name);
