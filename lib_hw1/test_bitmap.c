d4998b1d089d6df4085b35aff8aafb5d3bd4a7fc
sanglee <pksll325@gmail.com>
2019-09-29 22:54:51 -0700

    bitmap start

---
+++ lib_hw1/test_bitmap.c
@@ -0,0 +1,7 @@
+#include "testlib.h"
+#include "test_bitmap.h"
+
+void command_bitmap(char *command, char tokenize[MAX_INPUT_LEN][MAX_INPUT_LEN], int word_num) {
+	struct ds_table *target = NULL;
+
+}
---
+++ lib_hw1/test_bitmap.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "bitmap.h"
+
+void command_bitmap(char *command, char tokenize[MAX_INPUT_LEN][MAX_INPUT_LEN], int word_num);
