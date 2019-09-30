072ea5796cb6affde68d82162bf4439a70610a41
sanglee <pksll325@gmail.com>
2019-09-29 20:32:04 -0700

    CMD: apply, empty, size, clear pass

--- lib_hw1/test_hash.c
+++ lib_hw1/test_hash.c
@@ -18,6 +18,16 @@ void h_hash_free(struct hash_elem *e, void *aux) {
 	free(item);
 }
 
+void hash_act_sq(struct hash_elem *a,void *aux){
+	struct hash_item *item = hash_entry(a, struct hash_item ,elem);
+	item->data = item->data * item->data;
+}
+
+void hash_act_tri(struct hash_elem *a,void *aux){
+	struct hash_item *item = hash_entry(a, struct hash_item ,elem);
+	item->data = item->data * item->data * item->data;
+}
+
 void command_hash(char *command, char tokenize[MAX_INPUT_LEN][MAX_INPUT_LEN], int word_num) {
 	struct ds_table *target = NULL;
 
@@ -43,5 +53,44 @@ void command_hash(char *command, char tokenize[MAX_INPUT_LEN][MAX_INPUT_LEN], in
 		new->data = data;
 
 		hash_delete(target->hash, &(new->elem));
+	}
+
+	else if(!strcmp(command, "hash_apply")) {
+		target = find_ds_table(tokenize[1]);
+		if(!target) return;
+
+		if(!strcmp(tokenize[2], "square")) {
+			hash_apply(target->hash, &hash_act_sq);
+		}
+		else if(!strcmp(tokenize[2], "triple")) {
+			hash_apply(target->hash, &hash_act_tri);
+		}
+	}
+
+	else if(!strcmp(command, "hash_empty")) {
+		target = find_ds_table(tokenize[1]);
+		if(!target) return;
+
+		if(hash_empty(target->hash)) {
+			printf("true\n");
+		}
+		else {
+			printf("false\n");
+		}
+	}
+
+	else if(!strcmp(command, "hash_size")) {
+		target = find_ds_table(tokenize[1]);
+		if(!target) return;
+
+		int cnt = hash_size(target->hash);
+		printf("%d\n", cnt);
+	}
+
+	else if(!strcmp(command, "hash_clear")) {
+		target = find_ds_table(tokenize[1]);
+		if(!target) return;
+
+		hash_clear(target->hash, NULL);
 	}
 }
--- lib_hw1/test_hash.h
+++ lib_hw1/test_hash.h
@@ -7,6 +7,8 @@
 
 bool less_value_hash (const struct hash_elem *a, const struct hash_elem *b, void *aux);
 unsigned hash_value_elem(const struct hash_elem *e, void *aux); 
+void hash_act_sq(struct hash_elem *a,void *aux);
+void hash_act_tri(struct hash_elem *a,void *aux);
 
 void command_hash(char *command, char tokenize[MAX_INPUT_LEN][MAX_INPUT_LEN], int word_num);
 void h_hash_free(struct hash_elem *e, void *aux);
