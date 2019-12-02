9014c1515daa50f1e9a02a83fefd72ca579b33b7
sanglee <pksll325@gmail.com>
2019-12-02 09:17:33 -0800

    unnecessary functions deleted

--- pintos/src/threads/fixed_point.h
+++ pintos/src/threads/fixed_point.h
@@ -1,8 +1,6 @@
 #include <stdio.h>
 
 #define F (1 << 14)
-//#define INT_MAX((1 << 31) - 1)
-//#define INT_MIN(-(1 << 31))
 
 int int_to_fp(int n);
 int add_fp(int x, int y);
@@ -15,25 +13,15 @@ int div_fp(int x, int y);
 int div_mixed(int x, int n); 
 
 int int_to_fp(int n) {
-	return n*F;
+	return n * F;
 }
-
-/*
-int fp_to_int_round(int x) {
-	return (int) (x + 0.5);
-}
-
-int fp_to_int(int x) {
-	return (int) x;
-}
-*/
 
 int add_fp(int x, int y) {
 	return x + y;
 }
 
 int add_mixed(int x, int n) {
-	return x + n*F;
+	return x + n * F;
 }
 
 int sub_fp(int x, int y) {
@@ -41,17 +29,17 @@ int sub_fp(int x, int y) {
 }
 
 int sub_mixed(int x, int n) {
-	return x - n*F;
+	return n * F - x;
 }
 
 int mult_fp(int x, int y) {
-	return x * y;
+	int64_t tmp = x;
+	tmp = tmp * y / F;;
+	return (int)tmp;
 }
 
 int mult_mixed(int x, int n) {
-	int64_t tmp = x;
-	tmp = tmp * n / F;;
-	return (int)tmp;
+	return x * n;
 }
 
 int div_fp(int x, int y) {
